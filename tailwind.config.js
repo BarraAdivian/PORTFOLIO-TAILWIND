abb5eca647cfb4bae2771791dc89d31f751f2fd6
BarraAdivian <barra201209@student.abudzar.sch.id>
2025-11-13 09:38:06 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        warnautama: "#E63515",
+      },
+      animation: {
+        bounce: "bounce 0.3s infinite",
+      },
+    },
+  },
+  plugins: [],
+};
